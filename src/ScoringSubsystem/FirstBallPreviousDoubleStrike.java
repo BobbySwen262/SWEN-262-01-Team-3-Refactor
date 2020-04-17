925f870174581dcb4ca840627c081dd85d2fe3a5
Ryan Madulka <rtm1822@g.rit.edu>
2020-04-17 19:46:23 -0400

    merge conflict

--- src/ScoringSubsystem/FirstBallPreviousDoubleStrike.java
+++ src/ScoringSubsystem/FirstBallPreviousDoubleStrike.java
@@ -1,4 +1,24 @@
 package ScoringSubsystem;
 
-public class FirstBallPreviousDoubleStrike {
+import LanePinsetterSubsystem.Bowler;
+
+import java.util.HashMap;
+
+public class FirstBallPreviousDoubleStrike extends LaneScoreCase{
+    @Override
+    void score(HashMap<Bowler, int[][]> scoreCard, HashMap<Bowler, int[]> numericScores, Bowler currBowler, int frame) {
+        int currScore = scoreCard.get(currBowler)[frame][0];
+        numericScores.get(currBowler)[frame - 2] = 20+currScore;
+        if(frame >= 10){
+            if(currScore == 10){
+                scoreboard.setState(new TenthFrameStrikeNinthStrike());
+            } else {
+                scoreboard.setState(new TenthFrameStrikeNinthStrike());
+            }
+        } else {
+            if(currScore < 10){
+                scoreboard.setState(new SecondBallPreviousStrike());
+            }
+        }
+    }
 }

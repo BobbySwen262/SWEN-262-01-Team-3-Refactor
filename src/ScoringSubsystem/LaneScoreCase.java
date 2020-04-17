e3be2dd27b9413be8ceb938b4e416689c3500f36
Ryan Madulka <rtm1822@g.rit.edu>
2020-04-17 18:40:59 -0400

    LaneScoreCase interface updated

--- src/ScoringSubsystem/LaneScoreCase.java
+++ src/ScoringSubsystem/LaneScoreCase.java
@@ -1,4 +1,9 @@
 package ScoringSubsystem;
 
+import LanePinsetterSubsystem.Bowler;
+
+import java.util.HashMap;
+
 public interface LaneScoreCase {
+    void score(HashMap<Bowler, int[][]> scoreCard, HashMap<Bowler, int[]> numericScores, int frame);
 }

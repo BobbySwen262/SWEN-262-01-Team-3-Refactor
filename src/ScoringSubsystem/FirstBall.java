2676db271d3ecd76aac8bc2b950f8b42eec9639d
mje7835 <escalanteherzog@gmail.com>
2020-04-17 19:06:48 -0400

    firstBall has defined state transition to strike

--- src/ScoringSubsystem/FirstBall.java
+++ src/ScoringSubsystem/FirstBall.java
@@ -1,4 +1,22 @@
 package ScoringSubsystem;
 
-public class FirstBall {
+import LanePinsetterSubsystem.Bowler;
+
+import java.util.HashMap;
+
+public class FirstBall extends LaneScoreCase{
+
+    FirstBall(){
+
+    }
+    @Override
+    void score(HashMap<Bowler, int[][]> scoreCard, HashMap<Bowler, int[]> numericScores, Bowler currBowler, int frame) {
+        int currScore = scoreCard.get(currBowler)[frame][0];
+
+        if ( currScore < 10 ){
+            scoreboard.setState(new FirstBallPreviousStrike());
+        }else{
+            scoreboard.setState(new FirstBallPreviousStrike());
+        }
+    }
 }
--- src/ScoringSubsystem/FirstBallPreviousStrike.java
+++ src/ScoringSubsystem/FirstBallPreviousStrike.java
@@ -1,4 +1,12 @@
 package ScoringSubsystem;
 
-public class FirstBallPreviousStrike {
+import LanePinsetterSubsystem.Bowler;
+
+import java.util.HashMap;
+
+public class FirstBallPreviousStrike extends LaneScoreCase{
+    @Override
+    void score(HashMap<Bowler, int[][]> scoreCard, HashMap<Bowler, int[]> numericScores, Bowler currBowler, int frame) {
+
+    }
 }

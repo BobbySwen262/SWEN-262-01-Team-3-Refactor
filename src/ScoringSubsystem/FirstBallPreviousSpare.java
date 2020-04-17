dfa36878c326ff4a20074949cd17001693303919
Ryan Madulka <rtm1822@g.rit.edu>
2020-04-17 19:07:16 -0400

    SecondBall

--- src/ScoringSubsystem/FirstBallPreviousSpare.java
+++ src/ScoringSubsystem/FirstBallPreviousSpare.java
@@ -1,4 +1,12 @@
 package ScoringSubsystem;
 
-public class FirstBallPreviousSpare {
+import LanePinsetterSubsystem.Bowler;
+
+import java.util.HashMap;
+
+public class FirstBallPreviousSpare extends LaneScoreCase {
+    @Override
+    void score(HashMap<Bowler, int[][]> scoreCard, HashMap<Bowler, int[]> numericScores, Bowler currBowler, int frame) {
+
+    }
 }
--- src/ScoringSubsystem/SecondBall.java
+++ src/ScoringSubsystem/SecondBall.java
@@ -1,5 +1,19 @@
 package ScoringSubsystem;
 
-public class SecondBall {
+import LanePinsetterSubsystem.Bowler;
 
+import java.util.HashMap;
+
+public class SecondBall extends LaneScoreCase{
+    @Override
+    void score(HashMap<Bowler, int[][]> scoreCard, HashMap<Bowler, int[]> numericScores, Bowler currBowler, int frame) {
+        int pinsDownSecondBall = scoreCard.get(currBowler)[frame][1];
+        int pinsDownFirstBall = scoreCard.get(currBowler)[frame][0];
+        if(pinsDownFirstBall + pinsDownSecondBall == 10){
+            scoreboard.setState(new FirstBallPreviousSpare());
+        } else {
+            numericScores.get(currBowler)[frame] = pinsDownFirstBall+pinsDownSecondBall;
+            scoreboard.setState(new FirstBall());
+        }
+    }
 }

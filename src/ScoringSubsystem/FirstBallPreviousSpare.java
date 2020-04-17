4320c344362d4781d5486393e3ab3aa4b5adb783
Ryan Madulka <rtm1822@g.rit.edu>
2020-04-17 19:31:17 -0400

    Megre conflict

--- src/ScoringSubsystem/FirstBallPreviousSpare.java
+++ src/ScoringSubsystem/FirstBallPreviousSpare.java
@@ -7,6 +7,12 @@ import java.util.HashMap;
 public class FirstBallPreviousSpare extends LaneScoreCase {
     @Override
     void score(HashMap<Bowler, int[][]> scoreCard, HashMap<Bowler, int[]> numericScores, Bowler currBowler, int frame) {
-
+        int currScore = scoreCard.get(currBowler)[frame][0];
+        numericScores.get(currBowler)[frame-1]=10+currScore;
+        if(currScore == 10){
+            scoreboard.setState(new FirstBallPreviousStrike());
+        } else {
+            scoreboard.setState(new SecondBall());
+        }
     }
 }

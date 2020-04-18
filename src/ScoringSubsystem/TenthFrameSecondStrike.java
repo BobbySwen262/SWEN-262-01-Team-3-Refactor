71ddcc7513b1a1767ce439abdd70eb51da743e66
mje7835 <escalanteherzog@gmail.com>
2020-04-17 20:11:51 -0400

    TenthFrameStrikeNinthStrike

--- src/ScoringSubsystem/TenthFrameSecondStrike.java
+++ src/ScoringSubsystem/TenthFrameSecondStrike.java
@@ -7,6 +7,11 @@ import java.util.HashMap;
 public class TenthFrameSecondStrike extends LaneScoreCase{
     @Override
     void score(HashMap<Bowler, int[][]> scoreCard, HashMap<Bowler, int[]> numericScores, Bowler currBowler, int frame) {
-
+        int currScore = scoreCard.get(currBowler)[frame][2];
+        if (currScore >= 10){
+            scoreboard.setState(new TenthFrameEnd());
+        }else{
+            scoreboard.setState(new TenthFrameEnd());
+        }
     }
 }

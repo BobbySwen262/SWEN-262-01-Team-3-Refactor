d4a221426e02a83b8760192602edd5d04cbac413
Ryan Madulka <rtm1822@g.rit.edu>
2020-04-17 20:25:47 -0400

    help

--- src/ScoringSubsystem/TenthFrameSecondBallTenthStrike.java
+++ src/ScoringSubsystem/TenthFrameSecondBallTenthStrike.java
@@ -1,4 +1,12 @@
 package ScoringSubsystem;
 
-public class TenthFrameSecondBallTenthStrike {
+import LanePinsetterSubsystem.Bowler;
+
+import java.util.HashMap;
+
+public class TenthFrameSecondBallTenthStrike extends LaneScoreCase {
+    @Override
+    void score(HashMap<Bowler, int[][]> scoreCard, HashMap<Bowler, int[]> numericScores, Bowler currBowler, int frame) {
+        scoreboard.setState(new TenthFrameEnd());
+    }
 }

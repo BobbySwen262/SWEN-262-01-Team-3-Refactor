60417c800a1523b33a21147990e38d61f0ca71c5
Ryan Madulka <rtm1822@g.rit.edu>
2020-04-18 06:07:35 -0400

    TenthFrameEnd

--- src/ScoringSubsystem/TenthFrameEnd.java
+++ src/ScoringSubsystem/TenthFrameEnd.java
@@ -7,6 +7,21 @@ import java.util.HashMap;
 public class TenthFrameEnd extends LaneScoreCase{
     @Override
     void score(HashMap<Bowler, int[][]> scoreCard, HashMap<Bowler, int[]> numericScores, Bowler currBowler, int frame) {
-
+        int[] tenthFramePins = scoreCard.get(currBowler)[9];
+        int score = 0;
+        if(tenthFramePins[0]==10){
+            if(tenthFramePins[1]==10){
+                score = 30+3*tenthFramePins[2];
+            } else {
+                score = 10+2*(tenthFramePins[1]+tenthFramePins[2]);
+            }
+        } else {
+            if(tenthFramePins[0]+tenthFramePins[1]==10){
+                score = 10 + 2*(tenthFramePins[2]);
+            } else {
+                score = tenthFramePins[0]+tenthFramePins[1];
+            }
+        }
+        numericScores.get(currBowler)[9] = score;
     }
 }
